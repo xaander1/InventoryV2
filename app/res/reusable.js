05b387fda36c2005a5e32b6a12028817f8f44e13
xaander1 <alexnjogu52@gmail.com>
2019-12-07 10:45:31 +0300

    Completing settings module

---
+++ app/res/reusable.js
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------------------*/
+//initialise customers
+const init_data = Dexie => {
+var db = new Dexie('Data');
+db.version(1).stores({
+    	 regions: '++id,name',
+    	 departments: '++id,name',
+    	 os: '++id,name',
+    	 ms_office: '++id,name'
+    	});
+ return db;
+ }
+ /*--------------------------------------------------------------------*/
+ module.exports = {
+	init_data
+}

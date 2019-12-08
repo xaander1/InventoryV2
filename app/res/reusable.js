4d36356a06d8467bd07cad04758aa62d7438107d
Alexander <alexnjogu52@gmail.com>
2019-12-08 16:57:11 +0300

    Minor changes

--- app/res/reusable.js
+++ app/res/reusable.js
@@ -8,7 +8,7 @@ db.version(1).stores({
     	 os: '++id,name',
     	 ms_office: '++id,name',
     	 tech_name: '++id,name',
-    	 desktop:'++id,region,department,pc_model,pc_serial,monitor_model,monitor_serial,keyboard,mouse,comp_name,os,os_status,processor,ram,ms_office,ms_status,rem_storage,printer_model,printer_serial,scanner_model,scanner_serial,ups_model,tech_name,date,log'
+    	 desktops:'++id,region,department,pc_model,pc_serial,monitor_model,monitor_serial,keyboard,mouse,comp_name,os,os_status,processor,ram,ms_office,ms_status,rem_storage,printer_model,printer_serial,scanner_model,scanner_serial,ups_model,tech_name,date,log'
     	});
  return db;
  }

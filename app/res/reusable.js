e8c89923cbaf977cb56860da6647da4804bdb7ec
xaander1 <alexnjogu52@gmail.com>
2019-12-09 15:07:38 +0300

    Working on cameras module

--- app/res/reusable.js
+++ app/res/reusable.js
@@ -8,6 +8,8 @@ db.version(1).stores({
     	 os: '++id,name',
     	 ms_office: '++id,name',
     	 tech_name: '++id,name',
+    	 resolutions: '++id,name',
+    	 focals: '++id,name',
     	 desktops:'++id,region,department,pc_model,pc_serial,monitor_model,monitor_serial,keyboard,mouse,comp_name,os,os_status,processor,ram,ms_office,ms_status,rem_storage,printer_model,printer_serial,scanner_model,scanner_serial,ups_model,tech_name,date,log'
     	});
  return db;
